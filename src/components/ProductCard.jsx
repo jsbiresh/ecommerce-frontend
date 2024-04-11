aec5d75689b178b44af681165c9c28452ffa977b
Biresh <js.biresh@gmail.com>
2024-04-11 11:56:14 +0530

    fixed link issue

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -22,7 +22,7 @@ const ProductCard = (props) => {
           location.pathname === "/product" ? `gr-${grid}` : "col-3"
         }`}
       >
-        <Link to=":id" className="product-card position-relative">
+        <Link to=${location.pathname== '/' ? 'product/:id' : ''} className="product-card position-relative">
           <div className="wishlist-icon position-absolute">
             <button className="border-0 bg-transparent">
               <img src={wish} alt="wishlist" />
